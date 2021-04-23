36da94c9899ec2d431c8b65d1c14d13f859ba57f
Verenasworld <verena.felizidas@gmail.com>
2021-04-23 10:52:48 +0200

    add tab components and add to routing modules & set routerlink on tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bookmarks', component: BookmarksComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'todo', component: TodoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    BookmarksComponent,
+    NotesComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
