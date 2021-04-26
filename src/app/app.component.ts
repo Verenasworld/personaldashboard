7e947fbef933b0a0d7cdedbcde5f8261696a35f4
Verenasworld <verena.felizidas@gmail.com>
2021-04-26 16:57:23 +0200

    add active router snapshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
+import { trigger } from '@angular/animations';
 import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim',[])
+  ]
 })
 
 
@@ -13,5 +18,9 @@ export class AppComponent {
 
 
   theDate = new Date();
+
+  prepareRoute(outlet: RouterOutlet){
+    return outlet.activatedRoute.snapshot.url
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { BookmarkTitleComponent } from './bookmark-title/bookmark-title.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
