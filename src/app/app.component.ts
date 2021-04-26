9f61db6aff4f04db3bac3bc4eec8df437e765822
Verenasworld <verena.felizidas@gmail.com>
2021-04-26 10:36:29 +0200

    show tab-underline when tab is selected with opacity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'personaldashboard';
+  
+  theDate = new Date();
+  
+  
+  
 }
