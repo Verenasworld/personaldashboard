a7a5e81b92b34cf2209c5e7b351ef0bd4a81bef3
Verenasworld <verena.felizidas@gmail.com>
2021-04-27 10:38:54 +0200

    fix layout bug with position absolute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +12,20 @@ import { RouterOutlet } from '@angular/router';
   animations: [
     trigger('routeAnim',[
         transition('* => *', [
+          style({
+            position: 'relative'
+          }),
+
+          query(':enter, :leave' ,[
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%'
+            })
+
+          ],{optional: true}),
+
           query(':enter',[ 
             style({ opacity: 0 })
          ],{optional: true}),
