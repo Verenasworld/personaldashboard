acce4056acb18f783f98c0bdfb6a0852ad5cd104
Verenasworld <verena.felizidas@gmail.com>
2021-04-27 13:55:49 +0200

    set tab transition on increment & decrement for UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,48 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('routeAnim',[
-        transition('0 -> 1', [
+        transition(':increment', [
+          style({
+            position: 'relative',
+            overflow: 'hidden'
+          }),
+
+          query(':enter, :leave' ,[
+            style({
+
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%'
+            })
+
+          ],{optional: true}),
+
+    
+         group([
+          query(':leave',[
+            animate('300ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(100px)',
+            }))
+          ], {optional: true}),
+         
+          query(':enter', [
+            style({
+              opacity:0,
+              transform: 'translateX(-100px)'
+            }),
+            animate(500, style({
+              opacity: 1 ,
+              transform: 'translateX(0)'
+            
+            }))
+          ], { optional: true })
+         ])
+         
+        ]),
+        transition(':decrement', [
           style({
             position: 'relative',
             overflow: 'hidden'
