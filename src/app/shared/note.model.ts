6156a051b001ba3ada67d56af19e3e3db5bf5625
Verenasworld <verena.felizidas@gmail.com>
2021-04-28 11:07:23 +0200

    add methods retrieve & update note & specific note - to search array wit hid for a note

--- src/app/shared/note.model.ts
+++ src/app/shared/note.model.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export class Note{
     id:string
     
-   constructor( public title: string,public content: string){
+   constructor( public title: string, public content: string){
         this.id = uuidv4()
     }
 }
--- src/app/shared/note.service.ts
+++ src/app/shared/note.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Note } from './note.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
+  notes: Note[]
+
   constructor() { }
+
+  getNotes(){
+    return this.notes
+  }
+
+  getNote(id: string) {
+    this.notes.find(n => {
+      return n.id === id
+    })
+  }
 }
