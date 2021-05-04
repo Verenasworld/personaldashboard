a188a2bb52c9fc37a1e53ac6b1b96911027da846
Verenasworld <verena.felizidas@gmail.com>
2021-05-04 14:08:26 +0200

    style the basic add todo view , is the same how add note look likes

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-todo.component.scss']
 })
 export class AddTodoComponent implements OnInit {
+  showValidationErrors: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onFormSubmit(form: NgForm){
+
+  }
 }
