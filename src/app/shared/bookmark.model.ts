0ee8558f9a3552e7e12607238add7931eb7759a0
Verenasworld <verena.felizidas@gmail.com>
2021-05-05 16:24:03 +0200

    add bookmark service & model implement the service methods to get  add update delete

---
+++ src/app/shared/bookmark.model.ts
@@ -0,0 +1,15 @@
+
+import {v4 as uuid4} from 'uuid'
+
+export class Bookmark{
+    id: string
+    name: string
+    url: URL
+
+    constructor( name: string, url: string){
+          this.id = uuid4()  
+          this.url = new URL(url)
+          this.name = name
+    }
+}
+
