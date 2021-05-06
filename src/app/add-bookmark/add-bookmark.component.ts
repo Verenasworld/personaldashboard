dd2cec99cfb6c0df8a5cd91ff7ffd9b4a4093db9
Verenasworld <verena.felizidas@gmail.com>
2021-05-06 16:54:45 +0200

    add Bookmarkservice to add bookmark & set ngNativeValidate

--- src/app/add-bookmark/add-bookmark.component.ts
+++ src/app/add-bookmark/add-bookmark.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Bookmark } from '../shared/bookmark.model';
 import { BookmarkService } from '../shared/bookmark.service';
 
 
@@ -10,15 +11,16 @@ import { BookmarkService } from '../shared/bookmark.service';
   styleUrls: ['./add-bookmark.component.scss']
 })
 export class AddBookmarkComponent implements OnInit {
-  showValidationErrors: boolean = false
+  
 
   constructor(private bookmarkService: BookmarkService, private router: Router) { }
 
   ngOnInit(): void {
   }
   onFormSubmit(form: NgForm){
-    if(form.invalid) return this.showValidationErrors = true
-    //create an todo object and use the todo service to add the todo
+    const bookmark = new Bookmark(form.value.name, form.value.url)
+    this.bookmarkService.addBookmark(bookmark)
+
     this.router.navigateByUrl("/bookmarks")
     return
   }
