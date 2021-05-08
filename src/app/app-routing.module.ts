322fa37122b4deffcc22b16db15cfc02d1075268
Verenasworld <verena.felizidas@gmail.com>
2021-05-08 11:32:51 +0200

    add manage bookmarks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
-import { AppComponent } from './app.component';
+
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { CalenderComponent } from './calender/calender.component';
 import { ChatComponent } from './chat/chat.component';
@@ -15,10 +15,12 @@ import { NotesComponent } from './notes/notes.component';
 import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
+
   {path: 'bookmarks', component: BookmarksComponent, data:{ tabNum: 0}},
   {path: 'bookmarks/add', component: AddBookmarkComponent},
-  {path: 'bookmarks/manage', component: ManageBookmarksComponent, children:[
-    {path: ':id', component: EditBookmarkComponent},
+  {path: 'bookmarks/manage', component: ManageBookmarksComponent, 
+  children:[
+    {path: ':id', component: EditBookmarkComponent}
     ]},
   {path: 'todo', component: TodoComponent, data:{ tabNum: 1}},
   {path: 'todo/add', component: AddTodoComponent},
--- src/app/edit-bookmark/edit-bookmark.component.ts
+++ src/app/edit-bookmark/edit-bookmark.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Params, Router } from '@angular/router';
+import { BookmarksComponent } from '../bookmarks/bookmarks.component';
 import { Bookmark } from '../shared/bookmark.model';
 import { BookmarkService } from '../shared/bookmark.service';
 
@@ -13,14 +14,16 @@ export class EditBookmarkComponent implements OnInit {
 
   bookmark: Bookmark | undefined
 
+
   constructor(
       private bookmarkService : BookmarkService, 
-      private route: ActivatedRoute, ) { }
+      private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((paramMap: ParamMap)=> {
-      const bookmarkId = paramMap.get('id')
+     this.route.params.subscribe((params ) => {
+      const bookmarkId = params[':id']
       this.bookmark = this.bookmarkService.getBookmark(bookmarkId!)
+    
     })
   }
 
