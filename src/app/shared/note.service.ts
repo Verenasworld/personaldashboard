adc5d7e7ffa5c3d8e334ccd00eae686169ec3339
Verenasworld <verena.felizidas@gmail.com>
2021-05-11 11:05:24 +0200

    save state method and call it on add , updated and delete for localStorag

--- src/app/shared/note.service.ts
+++ src/app/shared/note.service.ts
@@ -5,7 +5,10 @@ import { Note } from './note.model';
   providedIn: 'root'
 })
 export class NoteService {
-  notes: Note[] = []
+  notes: Note[] = [
+    new Note ('title', 'thats working great'),
+    new Note ('easy', 'thats cool'),
+  ]
 
 
   constructor() {
@@ -26,12 +29,14 @@ export class NoteService {
   }
 
   addNote(note: Note){
-    this.notes.push(note)
+    this.notes.push(note);
+    this.saveState();
   }
 
   updateNote(id: string, updatedFields: Partial<Note>){
-    const note = this.getNote(id)
-    Object.assign(note, updatedFields)
+    const note = this.getNote(id);
+    Object.assign(note, updatedFields);
+    this.saveState();
   }
 
   //Die Methode Object.assign() kopiert die Werte aller aufzählbaren, eigenen Eigenschaften von einem oder mehreren Quellobjekten in ein Zielobjekt. Es wird das Zielobjekt zurückgegeben.
@@ -39,10 +44,24 @@ export class NoteService {
 deleteNote(id:string){
  const noteIndex = this.notes.findIndex(n => n.id === id )
  if(noteIndex == -1) return
- this.notes.splice(noteIndex, 1)
+ this.notes.splice(noteIndex, 1);
+ this.saveState();
 }
 //find the index of notes which has the id in the array 
 
 //Die Methode findIndex() gibt den Index des ersten Elements im Array zurück, das die bereitgestellte Testfunktion erfüllt. Andernfalls wird -1 zurückgegeben, um anzuzeigen, dass kein Element den Test bestanden hat.
 // splice()Method changes the content of an array by removing existing elements and / or adding new elements.
+
+
+
+
+
+//create Method that save the state from note array in local storage / store note array in local stroage / confert note array in json string / when we calling the saveState Method we will saving the 
+//notes array in localstorage as json string - each time wenn we add a note we want to saveState and each time when we want updateNote and also when we delete the note
+saveState(){
+ localStorage.setItem('notes', JSON.stringify(this.notes));
 }
+
+}
+
+
