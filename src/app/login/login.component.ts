d42ae241f81ea3635cbf4de9ece1ec03553fbf2f
Verenasworld <verena.felizidas@gmail.com>
2021-05-12 10:37:47 +0200

    twowaybinding with ngModel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string = ""
+  password: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log("Email:",this.email)
+    console.log("Password:",this.password)
   }
 
 }
